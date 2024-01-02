e5a97ed1229593bb69d2a8571e403305d844a2c5
chanoktrue <chanoktrue@me.com>
2024-01-02 16:58:33 +0700

    Initial Commit

---
+++ SF Symbols Demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SF Symbols Demo
+//
+//  Created by Thongchai Subsaidee on 2/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SF Symbols Demo/SF_Symbols_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SF_Symbols_DemoApp.swift
+//  SF Symbols Demo
+//
+//  Created by Thongchai Subsaidee on 2/1/24.
+//
+
+import SwiftUI
+
+@main
+struct SF_Symbols_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
